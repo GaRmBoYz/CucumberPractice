1550863c8c1c527283010e5fb81453cc6284da2c
GaRmBoYz <saleela@GaRmBoYzs-MacBook-Air.local>
2021-09-24 14:32:43 -0400

    CucumberPractice

---
+++ src/main/java/impl/HomeImpl.java
@@ -0,0 +1,33 @@
+package impl;
+
+import pages.HomePage;
+import utils.ConfigReader;
+import utils.WebDriverUtils;
+
+public class HomeImpl {
+    HomePage page;
+
+    public HomePage getPage(){
+        if(page == null)
+            page = new HomePage();
+        return page;
+    }
+
+    public void navigateToHomepage(){
+        WebDriverUtils.getDriver().get(ConfigReader.readProperty("url"));
+    }
+
+    public void openLinkInNewWindowAndSwitch(String linkText){
+        switch (linkText.toLowerCase()){
+            case "saucedemo": getPage().sauceDemoLink.click();
+                break;
+            default:
+                System.out.println("Invalid link text");
+        }
+
+        for(String eachWindow : WebDriverUtils.getDriver().getWindowHandles()){
+            if(!eachWindow.equals(WebDriverUtils.getDriver().getWindowHandle()))
+                WebDriverUtils.getDriver().switchTo().window(eachWindow);
+        }
+    }
+}
---
+++ src/main/java/impl/UserMgtImpl.java
@@ -0,0 +1,30 @@
+package impl;
+
+import org.junit.Assert;
+import pages.UserMgtPage;
+import utils.WebDriverUtils;
+
+public class UserMgtImpl {
+    private UserMgtPage page;
+
+    public UserMgtPage getPage() {
+        if(page == null){
+            page = new UserMgtPage();
+        }
+        return page;
+    }
+    public void openLinkInNewWindowAndSwitch(String linkText) {
+        switch (linkText) {
+            case "Access DB" : getPage().accessDbBtn.click();
+            break;
+            case "LOGIN" : getPage().loginBtn.click();
+            break;
+            default : System.out.println("Invalid link text");
+        }
+        for (String eachWindow : WebDriverUtils.getDriver().getWindowHandles()) {
+            if (!eachWindow.equals(WebDriverUtils.getDriver().getWindowHandle())) {
+                WebDriverUtils.getDriver().switchTo().window(eachWindow);
+            }
+        }
+    }
+}
